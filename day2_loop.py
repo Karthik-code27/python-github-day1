e7d6c6d3a608cf6e6a97c762d98b3aea4bea660f
Karthik <hmkarthik4935@gmail.com>
2025-07-02 15:36:05 +0530

    Loops!

---
+++ day2_loop.py
@@ -0,0 +1,17 @@
+count = 0
+
+while count < 5:
+     print ("Count is:", count)
+     count += 1
+
+print ("For loop with range")
+
+for i in range(5):
+         print ("i is:", i)
+         
+         
+print ("custom range")
+
+for i in range(1, 5):
+     print(i)
+     
